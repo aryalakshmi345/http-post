aca188c4d9c457c227e6971a5889d812492f2582
aryalakshmi345 <lekshmiarya113@gmail.com>
2024-11-03 07:38:55 +0530

    First-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <button (click)="addNewPost()">Add Post</button>
+  `
 })
 export class AppComponent {
-  title = 'http-post';
+  private apiUrl = 'http://localhost:3000/users';
+
+  constructor(private http: HttpClient) {}
+
+  addNewPost() {
+    const newPost = { title: 'Arya' };
+
+    this.http.post(this.apiUrl, newPost).subscribe(
+      response => {
+        console.log('Post created:', response);
+      },
+      error => {
+        console.error('Error creating post:', error);
+      }
+    );
+  }
 }
